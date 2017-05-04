252ca42385b7fd2340bf5fb10e05e8098d768eb1
David Guichon <davidrguichon@gmail.com>
2017-05-03 21:51:24 -0400

    Initial Commit

---
+++ Word Effects/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  Word Effects
+//
+//  Created by David Guichon on 2017-05-03.
+//  Copyright © 2017 David Guichon. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
